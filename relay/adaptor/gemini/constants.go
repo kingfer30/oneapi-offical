4877225c289c9ec3f84745a966812a7131cf8d8c
KingFer <kingfer30@qq.com>
2024-10-29 13:18:11 +0800

    新增gemini模型

--- relay/adaptor/gemini/constants.go
+++ relay/adaptor/gemini/constants.go
@@ -3,5 +3,24 @@ package gemini
 // https://ai.google.dev/models/gemini
 
 var ModelList = []string{
-	"gemini-pro", "gemini-1.0-pro", "gemini-1.5-flash", "gemini-1.5-pro", "text-embedding-004", "aqa",
+	"text-embedding-004",
+	"gemini-pro",
+	"gemini-pro-vision",
+	"gemini-1.0-pro",
+	"gemini-1.0-pro-001",
+	"gemini-1.0-pro-vision-001",
+	"gemini-1.0-pro-latest",
+	"gemini-1.5-pro",
+	"gemini-1.5-pro-001",
+	"gemini-1.5-pro-latest",
+	"gemini-1.5-pro-exp-0827",
+	"gemini-1.5-flash",
+	"gemini-1.5-flash-001",
+	"gemini-1.5-flash-latest",
+	"gemini-1.5-flash-exp-0827",
+	"gemini-1.5-flash-8b",
+	"gemini-1.5-flash-8b-001",
+	"gemini-1.5-flash-8b-latest",
+	"gemini-1.5-flash-8b-exp-0827",
+	"gemini-1.5-flash-8b-exp-0924",
 }
