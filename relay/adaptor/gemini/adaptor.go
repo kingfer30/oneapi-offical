d90ac094e94186d557d442103fef5068d7d5b402
KingFer <kingfer30@qq.com>
2024-12-01 16:41:19 +0800

    添加http代理

--- relay/adaptor/gemini/adaptor.go
+++ relay/adaptor/gemini/adaptor.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
 	"github.com/songquanpeng/one-api/common/config"
@@ -15,6 +16,8 @@ import (
 	"github.com/songquanpeng/one-api/relay/meta"
 	"github.com/songquanpeng/one-api/relay/model"
 	"github.com/songquanpeng/one-api/relay/relaymode"
+
+	commonClient "github.com/songquanpeng/one-api/common/client"
 )
 
 type Adaptor struct {
@@ -77,7 +80,50 @@ func (a *Adaptor) ConvertImageRequest(request *model.ImageRequest) (any, error) 
 }
 
 func (a *Adaptor) DoRequest(c *gin.Context, meta *meta.Meta, requestBody io.Reader) (*http.Response, error) {
-	return channelhelper.DoRequestHelper(a, c, meta, requestBody)
+	fullRequestURL, err := a.GetRequestURL(meta)
+	if err != nil {
+		return nil, fmt.Errorf("get request url failed: %w", err)
+	}
+	req, err := http.NewRequest(c.Request.Method, fullRequestURL, requestBody)
+	if err != nil {
+		return nil, fmt.Errorf("new request failed: %w", err)
+	}
+	err = a.SetupRequestHeader(c, req, meta)
+	if err != nil {
+		return nil, fmt.Errorf("setup request header failed: %w", err)
+	}
+	resp, err := doRequest(c, req)
+	if err != nil {
+		return nil, fmt.Errorf("do request failed: %w", err)
+	}
+	return resp, nil
+}
+func doRequest(c *gin.Context, req *http.Request) (*http.Response, error) {
+	var client *http.Client
+	if config.HttpProxy == "" {
+		client = commonClient.HTTPClient
+	} else {
+		logger.SysLogf("使用代理: %s ", config.HttpProxy)
+		url, err := url.Parse(config.HttpProxy)
+		if err != nil {
+			return nil, fmt.Errorf("url.Parse failed: %w", err)
+		}
+		client = &http.Client{
+			Transport: &http.Transport{
+				Proxy: http.ProxyURL(url),
+			},
+		}
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if resp == nil {
+		return nil, errors.New("resp is nil")
+	}
+	_ = req.Body.Close()
+	_ = c.Request.Body.Close()
+	return resp, nil
 }
 
 func (a *Adaptor) DoResponse(c *gin.Context, resp *http.Response, meta *meta.Meta) (usage *model.Usage, err *model.ErrorWithStatusCode) {
