457b82589701580056e3bbad0701b948b4bf0020
KingFer <kingfer30@qq.com>
2024-12-17 15:31:02 +0800

    新增模型

--- relay/adaptor/gemini/constants.go
+++ relay/adaptor/gemini/constants.go
@@ -12,6 +12,7 @@ var ModelList = []string{
 	"gemini-1.0-pro-latest",
 	"gemini-1.5-pro",
 	"gemini-1.5-pro-001",
+	"gemini-1.5-pro-002",
 	"gemini-1.5-pro-latest",
 	"gemini-1.5-pro-exp-0827",
 	"gemini-1.5-flash",
