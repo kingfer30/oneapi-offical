77f9a104f68fb67da3d31352edac7124cd868262
KingFer <kingfer30@qq.com>
2025-02-06 11:43:28 +0800

    新增pro模型

--- relay/adaptor/gemini/constants.go
+++ relay/adaptor/gemini/constants.go
@@ -36,4 +36,5 @@ var ModelList = []string{
 	"gemini-2.0-flash-thinking-exp-1219",
 	"gemini-2.0-flash-thinking-exp-01-21",
 	"gemini-2.0-flash-lite-preview-02-05",
+	"gemini-2.0-pro-exp-02-05",
 }
--- relay/billing/ratio/model.go
+++ relay/billing/ratio/model.go
@@ -121,6 +121,7 @@ var ModelRatio = map[string]float64{
 	"gemini-2.0-flash-thinking-exp":       1,
 	"gemini-2.0-flash-thinking-exp-01-21": 1,
 	"gemini-2.0-flash-lite-preview-02-05": 1,
+	"gemini-2.0-pro-exp-02-05":            1,
 	"aqa":                                 1,
 	// https://open.bigmodel.cn/pricing
 	"glm-4":         0.1 * RMB,
