69b3d8d1e88603c4334af19553a8d2805523f103
kingfer <kingfer30@qq.com>
2025-05-14 23:39:14 +0800

    新增gemini模型

--- relay/adaptor/gemini/constants.go
+++ relay/adaptor/gemini/constants.go
@@ -43,6 +43,8 @@ var ModelList = []string{
 	"embedding-001",
 	"gemini-2.5-pro-exp-03-25",
 	"gemini-2.5-flash-preview-04-17",
+	"gemini-2.5-pro-preview-03-25",
+	"gemini-2.5-pro-preview-05-06",
 }
 
 //定义支持画图的模型
